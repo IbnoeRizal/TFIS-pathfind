95f897525db4d4aa8c68a6c0435e90967eebaf8b
IbnoeRizal <ibnurizal849@gmail.com>
2026-06-12 16:43:53 +0700

    fix too much rerender

--- src/SearchTUI/pathList.tsx
+++ src/SearchTUI/pathList.tsx
@@ -12,6 +12,7 @@ import {
     useState,
     useEffect,
     useMemo,
+    useCallback,
 } from "react";
 
 import {type PackedOutRules, TsukamotoFISPathfinding } from "../classes/TFIS.js";
@@ -24,11 +25,82 @@ export default function PathList({pathListNode}:{pathListNode:graphLayout}){
     const listRef = useRef<ScrollListRef>(null);
     const query = useQuery();
 
+    
+    //render once this component is created
+    const rendereditems = useRef<React.ReactNode[]>(null);
+    //save the original source
+    const pathlist = useRef<PackedOutRules[] | string[]>(null);
+    //forceupdate
+    const [,forceUpdate] = useState(false);
+    
     const [error,setError] = useState<string | null>(null);
-    const [pathList,setPathlist] = useState<PackedOutRules[] | string[] | []>([]);
     const [selectedIndex, setIndex] = useState(0);
     const TFIS = useMemo(()=>new TsukamotoFISPathfinding(),[]);
     const timer = useRef<NodeJS.Timeout | null>(null);
+
+    const buildpathJsxElement = useCallback((raw:PackedOutRules[] | string[])=>{
+        rendereditems.current = raw.map((item,index)=>{
+            if(typeof item === 'string'){
+                return(
+                <Box key={item}>
+                    <Text color={"gray"} wrap="truncate-middle">
+                        {"  "}{ item }
+                    </Text>
+                </Box>)
+            }else{
+
+                const matchedidx = new Set(item.kemiripan.matched_idx);
+                return(
+                <Box key={item.path} alignItems="center" justifyContent="space-between">
+                    <Text color={"black"} wrap="truncate-middle">
+                        {"  "}
+                        {item.path.split("").map((char,i)=>
+                            matchedidx.has(i)?
+                            <Text key={i} bold color="yellowBright">{char}</Text>
+                            : <Text key={i}>{char}</Text>
+                        )}
+                        {item.crisp_out?.toFixed(2)}
+                    </Text>
+                </Box> )
+            }
+        })
+    },[])
+
+    const applySelect = useCallback((index:number,selected:boolean)=>{
+        const item = pathlist.current?.at(index);
+        if(!item) return;
+
+        const set_color = selected ? "green" : "black";
+        const set_prefix = selected ? ">  " : "   ";
+
+        let temp:React.ReactNode;
+        if(typeof item === 'string'){
+            temp =(
+                <Box key={item}>
+                    <Text color={set_color} wrap="truncate-middle">
+                        {set_prefix}{ item }
+                    </Text>
+                </Box>)
+        }else{
+            const matchedidx = new Set(item.kemiripan.matched_idx);
+            temp = (
+            <Box key={item.path} alignItems="center" justifyContent="space-between">
+                <Text color={set_color} wrap="truncate-middle">
+                    {set_prefix}
+                    {item.path.split("").map((char,i)=>
+                        matchedidx.has(i)?
+                        <Text key={i} bold color="yellowBright">{char}</Text>
+                        : <Text key={i}>{char}</Text>
+                    )}
+                    {item.crisp_out?.toFixed(2)}
+                </Text>
+            </Box> )
+        }
+
+        if(rendereditems.current){
+            rendereditems.current[index] = temp;
+        }
+    },[])
 
     useEffect(()=>{
         if(timer.current)clearTimeout(timer.current);
@@ -38,70 +110,60 @@ export default function PathList({pathListNode}:{pathListNode:graphLayout}){
             setError("base path atau query tidak boleh kosong")
         };
 
-       query.base && query.path 
-       &&(async()=>{
-            
+       query.base && query.path &&(async()=>{ 
             const result = (await TFIS.listFilesParallel(query.path,query.base))
                 .implication()
                 .defuzzyfication()
                 .ranking()
                 .getcopy()
-            
 
             if(result.length === 0){
                 timer.current && clearTimeout(timer.current);
                 setError("tidak ditemukan file atau folder");
                 timer.current = setTimeout(()=>setError(null),3000);
             }else{
-                setPathlist(result);
+                pathlist.current = result;
+                buildpathJsxElement(result);
+                applySelect(selectedIndex,true);
+                forceUpdate(p=>!p);
             }
         })();
     },[query.base,query.path]);
 
-    const renderedPathlist = useMemo(()=>{
-        const render = [];
-        for(const item of pathList){
-            if(typeof item === 'string') return null;
-
-            const matchedidx = new Set(item.kemiripan.matched_idx);
-            
-            render.push(
-                item.path.split("").map((char,i)=>
-                    matchedidx.has(i)?
-                    <Text key={i} bold color="yellowBright">{char}</Text>
-                    : <Text key={i}>{char}</Text>
-                )
-            )
-        }
-        
-        return render;
-    },[pathList]);
-
      // Handle keyboard navigation in the parent
     useInput((input, key) => {
-        if(!pathListNode.isActive()) return;
+        let nextindex:number = selectedIndex;
+        let len = pathlist.current?.length;
+
+        if(!len) return;
 
         switch (input) {
             case MOVEMENT.ATAS:
-                setIndex((prev) => Math.max(prev - 1, 0));
+                nextindex = Math.max(selectedIndex - 1, 0);
                 break;
             case MOVEMENT.BAWAH:
-                setIndex((prev) => Math.min(prev + 1, pathList.length - 1));
+                nextindex = Math.min(selectedIndex + 1, len - 1);
                 break;
             case MOVEMENT.TOP:
-                setIndex(0); // Jump to first
+                nextindex = 0
                 break;
             case MOVEMENT.BOTTOM:
-                setIndex(pathList.length -1); // Jump to last
+                nextindex= len -1; // Jump to last
                 break;
             default:
                 break;
         }
 
-        if (key.return) {
-            console.log(`Selected: ${pathList[selectedIndex]}`);
+        if(nextindex != selectedIndex){
+            applySelect(nextindex,true);
+            applySelect(selectedIndex,false);
+            setIndex(nextindex)
         }
-    });
+
+        if (key.return) {
+            console.log(`Selected: ${pathlist.current?.[selectedIndex]}`);
+        }
+    },{isActive:pathListNode.isActive()});
 
     return(
         <Box 
@@ -113,36 +175,12 @@ export default function PathList({pathListNode}:{pathListNode:graphLayout}){
                     {error}
                 </Text>
             </Box>
-            <ScrollList ref={listRef} selectedIndex={selectedIndex} backgroundColor={pathListNode.isActive() ? "black" : ""}>
-                {pathList.map((item,i)=>{
-                    const isString = typeof item == 'string';
-                    if(isString){
-                        return(
-                        <Box key={i}>
-                            <Text color={i === selectedIndex ? "green" : "gray"} wrap="truncate-middle">
-                                {i === selectedIndex ? "> " : "  "}
-                                { item }
-                            </Text>
-                        </Box>
-                        )
-                    }else{
-
-                        const selected = i === selectedIndex;
-                        const rank = item.crisp_out?.toFixed(0);
-
-                        return(
-                           <Box key={i} alignItems="center" justifyContent="space-between">
-                            <Text color={selected ? "green" : "white"} wrap="truncate-middle">
-                                {i === selectedIndex ? "> " : "  "}
-                                {renderedPathlist && renderedPathlist[i]}
-                            </Text>
-                            <Text color={selected ? "green" : "white"}>
-                               {rank}
-                            </Text>
-                        </Box> 
-                        )
-                    }
-                })}
+            <ScrollList 
+                ref={listRef} 
+                selectedIndex={selectedIndex} 
+                backgroundColor={pathListNode.isActive() ? "black" : ""}
+            >
+                {rendereditems.current}
             </ScrollList>
         </Box>
     )
